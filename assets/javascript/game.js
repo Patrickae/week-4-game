f729bd0496bb70cd15807218c8ad41f88a78bae4
Andy <Andy@Admins-MacBook-Air.local>
2017-03-13 01:05:16 -0400

    commit progress

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,21 +6,26 @@
 $(document).ready(function(){
 
 
-var isPlayerSelected = false;
-var isRyuSelected = false;
-var isChunLiSelected = false;
-var isKenSelected = false;
-var isEHondaSelected = false;
-var defenderNumber;
+
+var defenderNumber = "";
 var isDefenderSelected = false;
-var selectedNumber;
+var selectedNumber = "";
+var yourPlayerNumber = "";
+var yourOpponentNumber = "";
+
+var opponentAttack;
+var opponentHealth;
+var ogUserAttack=0;
+var userHealth;
+var newUserAttack = 0;
+var opponentsDefeated = 0;
 
 
 var players = [
-ryu = { name: "Ryu", healthPoints: 130, healthPointsName: "ryuHealth", counterAttackPower: 15, image:"ryu.png", value:"0" },
-chunLi = {name: "Chun-Li", healthPoints: 150, healthPointsName: "chunLiHealth", counterAttackPower:15, image:"chun-li.png", value:"1"},
-ken = {name: "Ken", healthPoints: 100, healthPointsName: "kenHealth", counterAttackPower: 15, image:"ken.png", value:"2"},
-eHonda = {name: "E-Honda", healthPoints: 180, healthPointsName: "eHondaHealth", counterAttackPower: 15, image:"e-honda.png", value:"3"}
+ryu = { name: "Ryu", healthPoints: 130, healthPointsName: "ryuHealth", counterAttackPower: 15, image:"ryu.png", value:"0", alive: true },
+chunLi = {name: "Chun-Li", healthPoints: 150, healthPointsName: "chunLiHealth", counterAttackPower:15, image:"chun-li.png", value:"1", alive: true},
+ken = {name: "Ken", healthPoints: 100, healthPointsName: "kenHealth", counterAttackPower: 15, image:"ken.png", value:"2", alive: true},
+eHonda = {name: "E-Honda", healthPoints: 180, healthPointsName: "eHondaHealth", counterAttackPower: 15, image:"e-honda.png", value:"3", alive: true}
 ];
 
 
@@ -28,6 +33,61 @@ console.log(players[0].name);
 
 
 
+
+function restart(){
+
+					players = [
+				ryu = { name: "Ryu", healthPoints: 130, healthPointsName: "ryuHealth", counterAttackPower: 15, image:"ryu.png", value:"0", alive: true },
+				chunLi = {name: "Chun-Li", healthPoints: 150, healthPointsName: "chunLiHealth", counterAttackPower:15, image:"chun-li.png", value:"1", alive: true},
+				ken = {name: "Ken", healthPoints: 100, healthPointsName: "kenHealth", counterAttackPower: 15, image:"ken.png", value:"2", alive: true},
+				eHonda = {name: "E-Honda", healthPoints: 180, healthPointsName: "eHondaHealth", counterAttackPower: 15, image:"e-honda.png", value:"3", alive: true}
+				];
+
+
+				var opponentAttack = 0;
+				var opponentHealth = 0;
+				var ogUserAttack=0;
+				var userHealth = 0;
+				var newUserAttack =0;
+				var opponentsDefeated = 0;
+
+				var isDefenderSelected = false;
+
+
+				var defenderNumber = "";
+				var selectedNumber = "";
+				var yourPlayerNumber = "";
+				var yourOpponentNumber = "";
+
+				$("#choose-opponent").hide();
+
+				$("#choose-character").show();
+
+
+
+				for(i=0; i<players.length; i++){
+
+					$(".player-"+i+"-name").text(players[i].name);
+
+					newImg = $("<img>");
+
+					newImg.attr("src", "assets/images/"+players[i].image);
+
+					$(".player-"+i+"-image").append(newImg);
+
+					$(".player-"+i+"-health").text(players[i].healthPoints);
+				};
+
+
+
+				$(".defender-info").empty();
+				$(".player-info").empty();
+				$(".opponent-info").empty();
+
+
+
+
+};
 
 
 for(i=0; i<players.length; i++){
@@ -54,50 +114,191 @@ $("#choose-opponent").hide();
 
 $(".potential-player").on("click", function(){
 
-	
-	console.log($(this).attr("value"));
 
 
-	yourPlayerNumber = $(this).attr("value");
 
-	$("#choose-character").hide();
-	$("#choose-opponent").show();
-
-	$(".my-player-name").text(players[yourPlayerNumber].name);
-
-	newImg = $("<img>");
-
-	newImg.attr("src", "assets/images/"+players[yourPlayerNumber].image);
-	$(".my-player-image").append(newImg);
-
-	$(".my-player-health").text(players[yourPlayerNumber].healthPoints);
+		console.log($(this).attr("value"));
 
 
-	$(".player"+yourPlayerNumber+"-option").hide();
+		yourPlayerNumber = $(this).attr("value");
+
+		$("#choose-character").hide();
+		$("#choose-opponent").show();
+
+		$(".my-player-name").text(players[yourPlayerNumber].name);
+
+		newImg = $("<img>");
+
+		newImg.attr("src", "assets/images/"+players[yourPlayerNumber].image);
+		$(".my-player-image").append(newImg);
+
+		$(".my-player-health").text(players[yourPlayerNumber].healthPoints);
+
+		$(".player"+yourPlayerNumber+"-option").hide();
+
+		console.log(isDefenderSelected);
+
+		
+
+		if (yourPlayerNumber === "0"){
+
+			players[yourPlayerNumber].attackPower = Math.floor((Math.random() * 15) + 20);
+		}
+		else if (yourPlayerNumber === "1"){
+
+			players[yourPlayerNumber].attackPower = Math.floor((Math.random() * 15) + 15);
+		}
+		else if (yourPlayerNumber === "2"){
+
+			players[yourPlayerNumber].attackPower = Math.floor((Math.random() * 15) + 30);
+		}
+		else if(yourPlayerNumber === "3"){
+
+			players[yourPlayerNumber].attackPower = Math.floor((Math.random() * 20) + 10);
+		};
+
+
+
+		console.log(players[yourPlayerNumber].attackPower);
+
 
 });
 
 $(".opponent").on("click", function(){
 
-	console.log($(this).attr("value"));
+	if(isDefenderSelected === false){
+			
+				isDefenderSelected = true;
+				console.log(isDefenderSelected);
 
-	yourOpponentNumber = $(this).attr("value");
+				console.log($(this).attr("value"));
 
-	$(".player"+yourOpponentNumber+"-option").hide();
+				yourOpponentNumber = $(this).attr("value");
 
-	$(".defender-name").text(players[yourOpponentNumber].name);
+				$(".player"+yourOpponentNumber+"-option").hide();
 
-	newImg = $("<img>");
+				$(".defender-name").text(players[yourOpponentNumber].name);
 
-	newImg.attr("src", "assets/images/"+players[yourOpponentNumber].image);
+				newImg = $("<img>");
 
-	$(".defender-image").append(newImg);
+				newImg.attr("src", "assets/images/"+players[yourOpponentNumber].image);
 
-	$(".defender-health").text(players[yourOpponentNumber].healthPoints);
+				$(".defender-image").append(newImg);
+
+				$(".defender-health").text(players[yourOpponentNumber].healthPoints);
+
+					if (yourOpponentNumber === "0"){
+
+						players[yourOpponentNumber].counterAttackPower = Math.floor((Math.random() * 15) + 5);
+					}
+					else if (yourOpponentNumber === "1"){
+
+						players[yourOpponentNumber].counterAttackPower = Math.floor((Math.random() * 10) + 5);
+					}
+					else if (yourOpponentNumber === "2"){
+
+						players[yourOpponentNumber].counterAttackPower = Math.floor((Math.random() * 15) + 8);
+					}
+					else if(yourOpponentNumber === "3"){
+
+						players[yourOpponentNumber].counterAttackPower = Math.floor((Math.random() * 10) + 5);
+					};
+
+
+
+
+
+
+
+
+				players[yourOpponentNumber].counterAttackPower = Math.floor((Math.random() * 30) + 15);
+
+	}
+});
+
+
+$("#attack-btn").on("click", function(){
+
+	if (isDefenderSelected === true){
+
+
+			console.log(players[yourOpponentNumber].counterAttackPower);
+
+			opponentAttack = players[yourOpponentNumber].counterAttackPower;
+			opponentHealth = players[yourOpponentNumber].healthPoints;
+
+			// console.log(opponentAttack);
+			// console.log(opponentHealth);
+
+			ogUserAttack = players[yourPlayerNumber].attackPower;
+			userHealth = players[yourPlayerNumber].healthPoints;
+
+
+
+			// console.log(ogUserAttack);
+			// console.log(userHealth);
+
+			
+			newUserAttack += ogUserAttack;
+
+			console.log(newUserAttack);
+
+
+			
+			opponentHealth -= newUserAttack;
+
+
+			if(opponentHealth > 0){
+
+			userHealth -= opponentAttack;
+		};
+
+			players[yourOpponentNumber].healthPoints = opponentHealth;
+			players[yourPlayerNumber].healthPoints = userHealth;
+
+
+
+			$(".defender-health").text(players[yourOpponentNumber].healthPoints);
+			$(".my-player-health").text(players[yourPlayerNumber].healthPoints);
+
+
+
+
+	};
+
+				if( opponentHealth < 1){
+
+					players[yourOpponentNumber].alive = false;
+					isDefenderSelected = false;
+
+					opponentsDefeated += 1;
+
+					$(".defender-info").empty();
+			};
+
+
+
+			if( userHealth < 1) {
+				 alert("You have been defeated");
+
+				 restart();
+
+			};
+
+			
+			if(opponentsDefeated === 3){
+
+				alert("You are the champion!");
+
+				restart();
+			};
+
+
+	$("#reset-btn").on("click", restart());
+
 
 
 });
-
 
 
 // function updateDom( div, v){
